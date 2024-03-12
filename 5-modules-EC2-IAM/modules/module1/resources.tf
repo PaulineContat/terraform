95d72931ff6d44c68d7904b7a894ee05a48392ae
EC2 Default User <ec2-user@ip-172-31-71-20.ec2.internal>
2024-03-12 13:34:44 +0000

    oui

--- 5-modules-EC2-IAM/modules/module1/resources.tf
+++ 5-modules-EC2-IAM/modules/module1/resources.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "ec2_instance" {
   ami           = data.aws_ami.linux.id
-  instance_name = var.instance_name
   instance_type = var.instance_type
   instance_role = data.aws_iam_role.lab_role.name
+  tags = {
+    Name = var.instance_name
+  }
+  iam_instance_profile = data.aws_iam_role.lab_role.name
 }
