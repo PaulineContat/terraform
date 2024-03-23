5048af2644fe10c566539ddfac0e8f696614fb4c
EC2 Default User <ec2-user@ip-172-31-42-173.ec2.internal>
2024-03-23 22:26:33 +0000

    modifs

--- 5-modules-EC2-IAM/modules/lambda/resources.tf
+++ 5-modules-EC2-IAM/modules/lambda/resources.tf
@@ -2,8 +2,12 @@ resource "aws_lambda_function" "lambda_function" {
   filename         = "${path.module}/lambda_function_payload.zip"
   function_name    = var.lambda_function_name
   description      = var.lambda_description
-  role             = var.lambda_role_arn
+  role             = local.lambda_role_arn
   handler          = format("%s.handler", var.lambda_function_name)
   source_code_hash = filebase64sha256("./modules/lambda/lambda_function_payload.zip")
   runtime          = var.lambda_runtime
+}
+
+locals {
+  lambda_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 }
