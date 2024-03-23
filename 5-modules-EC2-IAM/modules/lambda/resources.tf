60afdddfa755c3537efbc3285b530f3f32863f8b
EC2 Default User <ec2-user@ip-172-31-42-173.ec2.internal>
2024-03-23 23:12:50 +0000

    modifs

--- 5-modules-EC2-IAM/modules/lambda/resources.tf
+++ 5-modules-EC2-IAM/modules/lambda/resources.tf
@@ -1,10 +1,25 @@
+resource "null_resource" "lambda_exporter" {
+  triggers = {
+    index = "${base64sha256(file("${path.module}/send-mail.py"))}"
+  }
+}
+
+data "archive_file" "lambda_exporter" {
+  output_path = "${path.module}/lambda_function_payload.zip"
+  source {
+    content  = "1"
+    filename = "send-mail.py"
+  }
+  type        = "zip"
+}
+
 resource "aws_lambda_function" "lambda_function" {
   filename         = "${path.module}/lambda_function_payload.zip"
   function_name    = var.lambda_function_name
   description      = var.lambda_description
   role             = local.lambda_role_arn
   handler          = format("%s.handler", var.lambda_function_name)
-  source_code_hash = filebase64sha256("./modules/lambda/lambda_function_payload.zip")
+  source_code_hash = "${data.archive_file.lambda_exporter.output_base64sha256}"
   runtime          = var.lambda_runtime
 }
 
